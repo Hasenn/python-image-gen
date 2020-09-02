5b351a367d275d2c5575e79665d81e1c125e8930
eliott veyrier <eliottarloveyrier@live.fr>
2020-09-02 23:20:12 +0200

    Refactored to OOP, generalized,  and moved old code to diff_test.py

--- diffusion.py
+++ diffusion.py
@@ -1,50 +1,88 @@
+import argparse
 import random
 import ntpath
 from PIL import Image
 from copy import deepcopy
 
-N_STEPS = 15
-SIZE = 100
+def clamp(x, smallest, largest): return max(smallest, min(x, largest))
 
-def clamp(n, smallest, largest): return max(smallest, min(n, largest))
+class Automaton:
+    
+    def __init__(self):
+        self.probabilities = [
+            [0.1,  0.5, 0.1 ],
+            [0.1,  0.1,  0.1 ],
+            [0.05, 0.1,  0.05]]
+        self.values = [
+            [1,  1,  1],
+            [1,  0,  1 ],
+            [1, 0,  1]]
+        self.seeds = set()
+        self.p_direct = 0.2
+        self.seed_value = 5
+    def cell_step(self, grid, i, j, neighbours):
+        s = 0
+        for (k,l) in neighbours:
+            s += int(random.random() < self.probabilities[k-i][l-j]) * self.values[k-i][l-j] * grid[k][l]
+            if (k,l) in self.seeds:
+                s += int(random.random() < self.p_direct) * self.seed_value
+        return clamp(s,0,255)
 
-def get_moore_neighbourhood(i, j):
-    return filter(lambda x : (0 <= x[0] < SIZE) and (0 <= x[1] < SIZE) ,[(i + k, j+l) for k in [-1,0,1] for l in [-1,0,1]])
 
-def next_step(grid, seeds, p_direct, p_indirect):
-    new_grid = deepcopy(grid)
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            for k,l in get_moore_neighbourhood(i,j):
-                if (k,l) in seeds:
-                    new_grid[i][j] += clamp(int(random.random() < p_direct) * 15, 0, 255)
-                else:
-                    new_grid[i][j] +=  clamp(int(random.random() < p_indirect) * new_grid[k][l], 0, 255)
-    return new_grid
+        
 
-def main():
-    img = Image.new("L", (SIZE,SIZE))
+class Simulation:
+    def __init__(self, size_x, size_y):
+        self.size_x = size_x
+        self.size_y = size_y
+        self.grid = [[0 for i in range(size_y)] for j in range(size_x)]
+        self.automaton = Automaton()
+
+    def get_moore_neighbourhood(self, i, j):
+        return filter(
+            lambda x : (0 <= x[0] < self.size_x) and (0 <= x[1] < self.size_y) ,
+            [(i + k, j+l) for k in [-1,0,1] for l in [-1,0,1]])
+    
+    def sim_step(self):
+        new_grid = deepcopy(self.grid)
+        for i in range(self.size_x):
+            for j in range(self.size_y):
+                new_grid[i][j] = clamp(
+                    new_grid[i][j] + self.automaton.cell_step(self.grid, i, j, self.get_moore_neighbourhood(i,j)),
+                    0, 255)
+        return new_grid
+    
+    def advance(self, n):
+        for i in range(n):
+            print(f"Computing step {i}")
+            self.grid = self.sim_step()            
+
+    
+    def render(self):
+        img = Image.new("L", (self.size_x, self.size_y))
+        for i in range(self.size_x):
+            for j in range(self.size_y):
+                img.putpixel((i,j), self.grid[i][j])
+        return img
+
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("steps", type=int)
+    args = parser.parse_args()
+
     random.seed()
 
-    seeds = set()
+    sim = Simulation(100,100)
+
     for i in range(200):
-        k,l = random.randint(0,SIZE), random.randint(0,SIZE)
-        if not ((k,l) in seeds):
-            seeds.add((k,l))
-    
-    print(seeds)
-    grid = [[0 for i in range(SIZE)] for j in range(SIZE)]
-    p_direct = 0.1
-    p_indirect = 0.05
+        k,l = random.randint(0,sim.size_x), random.randint(0,sim.size_y)
+        if not ((k,l) in sim.automaton.seeds):
+            sim.automaton.seeds.add((k,l))
 
-    for i in range(N_STEPS):
-        grid = next_step(grid,seeds,p_direct,p_indirect)
-    
-    for i in range(SIZE):
-        for j in range(SIZE):
-            img.putpixel((i,j),grid[i][j])
+    sim.advance(args.steps)
 
-    img.save(ntpath.dirname(__file__) + "/diffusion.png")
+    img = sim.render()
+    path = ntpath.abspath(__file__)
+    img.save(ntpath.dirname(path) + "/" + "diff6.png")
 
-if __name__ == "__main__":
-    main()
